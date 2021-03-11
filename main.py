d8384f2c759df4df62d4fa10d18e0c689d16d9e8
Lucy Acheson <70671534+lucyach@users.noreply.github.com>
2021-03-11 10:07:31 -0800

    Update main.py
    "blacklist" list names not inconsistent naming conventions also camel case'd it all

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 violations = {}
-bad_words = ["fuck", "bitch", "cunt", "pussy"]
-specialBadWords = ["queer", "retard", "fag", "tranny"]
+blacklist = ["fuck", "bitch", "cunt", "pussy"]
+specialBlacklist = ["queer", "retard", "fag", "tranny"]
 specialResponses = [", please avoid using that language if you are cishet.", ", please avoid using that language if you are abled.", ", please avoid using that language if you are heterosexual.", ", please avoid using that language if you are cisgender."]
 specialSnitch = [". Note that this word is special and it is okay for LGBT+ people to use.", ". This is their violation #' + str(violations[message.author]) + '. Note that this word is special and it is okay for disabled people to use.", ". Note that this word is special and it is okay for homosexual people to use.", ". Note that this word is special and it is okay for transgender people to use."]
 
@@ -20,8 +20,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    for i in range(0, len(bad_words)):
-      if bad_words[i] in msg:
+    for i in range(0, len(blacklist)):
+      if blacklist[i] in msg:
         await message.delete()
         await message.channel.send(str(message.author) + ", please avoid using that language.")
         if message.author not in violations:
@@ -32,8 +32,8 @@ async def on_message(message):
           channel = client.get_channel(783529978532593685) ## replace this with your staff channel id
           await channel.send('Snitch message: ' + str(message.author) + ' said "' + str(msg) + '" in channel #' + str(message.channel) + '. This is their violation #' + str(violations[message.author]))
     
-    for i in range(0, len(specialBadWords)):
-      if specialBadWords[i] in msg:
+    for i in range(0, len(specialBlacklist)):
+      if specialBlacklist[i] in msg:
         await message.delete()
         if i == 0:
           await message.channel.send(str(message.author) + specialResponses[i])
